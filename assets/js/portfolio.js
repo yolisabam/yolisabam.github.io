da9acd1e8f1ea627a1f0af3ba945aa9271fbaa0b
Yolisa Bam <yolisabam@Yolisas-MacBook-Pro.local>
2017-11-24 14:25:22 -0800

    adding portfolio section

---
+++ assets/js/portfolio.js
@@ -0,0 +1,22 @@
+window.__portfolio = [
+  {
+    image: "assets/images/gifgirl.png",
+    albumTiitle: "GIRL GIF GANG",
+    releaseDate: "HTML/CSS, JQUERY, JAVASCRIPT, GIPHY API",
+  },
+  {
+    image: "assets/images/utender.png",
+    albumTiitle: "UTENDER",
+    releaseDate: "HTML/CSS, JQUERY, JAVASCRIPT, API",
+  },
+  {
+    image: "assets/images/friend-finder.png",
+    albumTiitle: "FRIEND FINDER",
+    releaseDate: "HANDLEBARS, HEROKU, NODEJS, CSS",
+  },
+  {
+    image: "assets/images/aep.png",
+    albumTiitle: "ALCOHOL EVENT PLANNER",
+    releaseDate: "MYSQL, HANDLEBARS, CSS, JAVASCRIPT, BOOTSTRAP, JQUERY, NODE,JS",
+  }
+]  
