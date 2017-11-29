5deef62e025bc9dd5a5854fe50153a71cb30e00b
Yolisa Bam <yolisabam@calvisitor-10-105-168-29.calvisitor.1918.berkeley.edu>
2017-11-28 17:57:49 -0800

    website updated

---
+++ js/portfolio.js
@@ -0,0 +1,22 @@
+window.__portfolio = [
+  {
+    image: "img/gifgirl.png",
+    albumTiitle: "DAMN.",
+    releaseDate: "APRIL 14, 2017",
+  },
+  {
+    image: "img/utender.png",
+    albumTiitle: "UNTITLED UNMASTERED",
+    releaseDate: "MARCH 4, 2016",
+  },
+  {
+    image: "img/friend-finder.png",
+    albumTiitle: "TO PIMP A BUTTERFLY",
+    releaseDate: "MARCH 15, 2015",
+  },
+  {
+    image: "img/aep.png",
+    albumTiitle: "TO PIMP A BUTTERFLY",
+    releaseDate: "MARCH 15, 2015",
+  }
+]
