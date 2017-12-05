76e668570246dabac12575722202dd349bece972
Yolisa Bam <yolisabam@Yolisas-MacBook-Pro.local>
2017-12-04 23:59:20 -0800

    formatting JS file

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,24 +3,24 @@ window.__portfolio = [
     image: "img/gif-generator.png",
     albumTiitle: "RANDOM GIF GENERATOR.",
     releaseDate: "JS, JQUERY, HTML, CSS",
-    buyLink: "https://github.com/yolisabam/random-gif-generator"
+    buyLink: "https://github.com/yolisabam/random-gif-generator",
   },
   {
     image: "img/utender.png",
     albumTiitle: "UTENDER",
     releaseDate: "JS, API, JQUERY, HTML, CSS",
-    buyLink:""
+    buyLink:"",
   },
   {
     image: "img/siri-clone.png",
     albumTiitle: "SIRI CLONE BOT",
     releaseDate: "JS, NODE.JS, API",
-    buyLink:""
+    buyLink:"",
   },
   {
     image: "img/event-choice.png",
     albumTiitle: "THIS.EVENT:CHOICE",
     releaseDate: "JS, HANDLEBARS, MYSQL, CSS",
-    buyLink: ""
+    buyLink: "",
   }
 ]
